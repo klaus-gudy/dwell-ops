db20958820d5cf8694c4a1e487bbb96e1a53b1c9
Klaus-gudy <klausongudy@gmail.com>
2025-04-20 17:22:12 +0300

    Added schema responsible for registration of users

---
+++ schemas/index.ts
@@ -0,0 +1,7 @@
+import * as z from 'zod';
+
+export const RegisterSchema = z.object({
+    name: z.string().min(1, { message: 'Name is required' }),
+    email: z.string().email({ message: 'Please enter a valid email' }),
+    password: z.string().min(6, { message: 'Password must be at least 6 characters long' }),
+})
