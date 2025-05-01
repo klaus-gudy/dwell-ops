52d86a4e78bb770654ff253324fca1388b8ae7ee
Klaus-gudy <klausongudy@gmail.com>
2025-05-01 19:58:57 +0300

    Implement JWT and session callbacks for NextAuth integration

--- auth.ts
+++ auth.ts
@@ -2,9 +2,43 @@ import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { prisma } from '@/lib/prisma';
 import authConfig from './auth.config';
+import { getAccountByUserId } from './services/accounts';
+import { getUserById } from './services/user';
 
 export const { handlers: { GET, POST }, signIn, signOut, auth } = NextAuth({
     adapter: PrismaAdapter(prisma),
     session: { strategy: 'jwt' },
     ...authConfig,
+    callbacks:{
+        // async signIn({ user, account }) {
+        //     if (account?.provider === 'credentials') {
+        //         return true;
+        //     }
+        //     const existingUser =  await getUserById(user.id ?? '');
+        //     if (!existingUser?.emailVerified){
+        //         return false;
+        //     }
+        //     return true;
+        // },
+        async jwt({ token, user }) {
+            if (user) {
+                token.sub = user.id;
+            }
+
+            const existingAccount = token.sub ? await getAccountByUserId(token.sub) : null;
+            if (existingAccount) {
+                token.isOauth = !!existingAccount;
+            }
+            
+            console.log('JWT Callback:', token);
+            return token;
+        },
+        async session({ session, token }) {
+            if (token.sub) {
+                session.user.id = token.sub;
+            }
+            console.log('Session Callback:', token);
+            return session;
+        }
+    }
 })
