db9412dbde9892145394ca3f25f971035c2df98d
Klaus-gudy <klausongudy@gmail.com>
2025-06-24 23:57:48 +0300

    Implemented complex data table with tenant management feature

--- app/(protected)/tenant/page.tsx
+++ app/(protected)/tenant/page.tsx
@@ -1,6 +1,58 @@
-export default function TenantsPage() {
+import { DataTable } from "./components/data-table";
+import { columns } from "./components/columns";
+
+async function getTasks() {
+  return [
+    {
+        "id": "TASK-8782",
+        "title": "You can't compress the program without quantifying the open-source SSD pixel!",
+        "status": "in progress",
+        "label": "documentation",
+        "priority": "medium"
+      },
+      {
+        "id": "TASK-7878",
+        "title": "Try to calculate the EXE feed, maybe it will index the multi-byte pixel!",
+        "status": "backlog",
+        "label": "documentation",
+        "priority": "medium"
+      },
+      {
+        "id": "TASK-7839",
+        "title": "We need to bypass the neural TCP card!",
+        "status": "todo",
+        "label": "bug",
+        "priority": "high"
+      },
+      {
+        "id": "TASK-5562",
+        "title": "The SAS interface is down, bypass the open-source pixel so we can back up the PNG bandwidth!",
+        "status": "backlog",
+        "label": "feature",
+        "priority": "medium"
+      },
+      {
+        "id": "TASK-8686",
+        "title": "I'll parse the wireless SSL protocol, that should driver the API panel!",
+        "status": "canceled",
+        "label": "feature",
+        "priority": "medium"
+      },
+      {
+        "id": "TASK-1280",
+        "title": "Use the digital TLS panel, then you can transmit the haptic system!",
+        "status": "done",
+        "label": "bug",
+        "priority": "high"
+      }
+]
+}
+
+export default async function TenantsPage() {
+  const tasks = await getTasks()
     return (
-      <div className="flex flex-col gap-2 py-2 md:gap-2 md:py-4">
+      <div className="flex flex-col gap-2 px-4 py-2 md:gap-2 md:py-4 lg:px-6">
+        <DataTable data={tasks} columns={columns} />
       </div>
     )
   }
