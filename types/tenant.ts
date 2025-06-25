b5b7b1bd089ff1b50831ae21b8ae4c8db6146aad
Klaus-gudy <klausongudy@gmail.com>
2025-06-25 23:52:44 +0300

    Add phoneNo field to Tenant interface for improved tenant data management

--- types/tenant.ts
+++ types/tenant.ts
@@ -3,3 +3,15 @@ export interface SelectTenant {
     name: string;
     email: string;
 }
+
+export interface Tenant {
+    id: string
+    name: string
+    phoneNo: string
+    email: string
+    status: string
+    unitName: string
+    propertyName: string
+    leaseEndingDate: Date | null
+    createdAt: Date
+  }
