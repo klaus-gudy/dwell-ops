43494fecf6985a1916254a59143d48e1a70595f2
Klaus-gudy <klausongudy@gmail.com>
2025-06-28 23:43:54 +0300

    Added TenantDetailsPage component for displaying tenant information

---
+++ app/(protected)/tenant/[id]/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+const TenantDetailsPage = () => {
+
+
+    return (
+        <div>
+            <h1>Tenant Details</h1>
+        </div>
+    );
+};
+
+export default TenantDetailsPage;
