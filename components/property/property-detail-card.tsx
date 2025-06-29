1c19e5ef89c131ee5a37ae519f2a7fe0d114e733
Klaus-gudy <klausongudy@gmail.com>
2025-06-29 17:10:34 +0300

    💫Enhanced property overview card for consistency and readability
    - Added separators and organized with precision property-detail-card

--- components/property/property-detail-card.tsx
+++ components/property/property-detail-card.tsx
@@ -3,107 +3,106 @@ import { Separator } from "../ui/separator";
 import { Property } from "@/types/property";
 import { Button } from "../ui/button";
 
-export default function PropertyOverview({ property }: {property: Property}) {
-
+export default function PropertyOverview({ property }: { property: Property }) {
   return (
-    <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
-      <div className="space-y-6 lg:col-span-2">
-        <Card className="gap-0 shadow-none">
-          <CardHeader>
-            <CardTitle className="flex items-center text-md">
-              Property details
-            </CardTitle>
-          </CardHeader>
-          <CardContent className="space-y-4 text-sm">
-            <p className="text-muted-foreground text-md">{property.description}</p>
-            <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-              <div className="space-y-2">
-                <div className="flex items-center justify-between">
-                  <span className="font-medium">Building type:</span>
-                  <span>{property.buildingType}</span>
-                </div>
-                <Separator />
-                <div className="flex items-center justify-between">
-                  <span className="font-medium">Year built:</span>
-                  <span className="flex items-center">
-                    {property.yearBuilt}
-                  </span>
-                </div>
-                <Separator />
-                <div className="flex items-center justify-between">
-                  <span className="font-medium">Total area(sq ft):</span>
-                  <span className="flex items-center">
-                    {property.areaFootage || "N/A"}
-                  </span>
-                </div>
-                <Separator />
-                <div className="flex items-center justify-between">
-                  <span className="font-medium">Number of floors:</span>
-                  <span>{property.numberOfFloors || "N/A"}</span>
-                </div>
-                <Separator />
-                <div className="flex items-center justify-between">
-                  <span className="font-medium">Number of parking spaces:</span>
-                  <span className="flex items-center">
-                    {property.numberOfParkingSpaces || "N/A"}
-                  </span>
-                </div>
-              </div>
+    <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
+      {/* Property Information */}
+      <Card className="gap-0 shadow-none">
+        <CardHeader>
+          <CardTitle className="flex items-center text-md">
+            Property details
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="space-y-4 text-sm">
+          <p className="text-muted-foreground text-md">
+            {property.description}
+          </p>
+          <div className="grid grid-cols-1 space-y-2">
 
-              <div className="space-y-2">
-                <div className="flex items-center justify-between">
-                  <span className="font-medium">Street:</span>
-                  <span className="flex items-center"> {property.address || "N/A"}
-                  </span>
-                </div>
-                <Separator />
-                <div className="flex items-center justify-between">
-                  <span className="font-medium">District:</span>
-                  <span>{property.district}</span>
-                </div>
-                <Separator />
-                <div className="flex items-center justify-between">
-                  <span className="font-medium">City:</span>
-                  <span>{property.city}</span>
-                </div>
+              <div className="flex items-center">
+                <label className="w-1/2 text-sm font-medium text-muted-foreground">Building type:</label>
+                <p className="w-1/2 text-sm">{property.buildingType}</p>
+              </div>
+              <Separator />
+              <div className="flex items-center">
+                <label className="w-1/2 text-sm font-medium text-muted-foreground">Year built:</label>
+                <p className="w-1/2 text-sm">{property.yearBuilt}</p>
+              </div>
+              <Separator />
+              <div className="flex items-center">
+                <label className="w-1/2 text-sm font-medium text-muted-foreground">Total area(sq ft):</label>
+                <p className="w-1/2 text-sm">
+                  {property.areaFootage || "N/A"}
+                </p>
+              </div>
+              <Separator />
+              <div className="flex items-center">
+                <label className="w-1/2 text-sm font-medium text-muted-foreground">Number of floors:</label>
+                <p className="w-1/2 text-sm">{property.numberOfFloors || "N/A"}</p>
+              </div>
+              <Separator />
+              <div className="flex items-center">
+                <label className="w-1/2 text-sm font-medium text-muted-foreground">Number of parking spaces:</label>
+                <p className="w-1/2 text-sm">
+                  {property.numberOfParkingSpaces || "N/A"}
+                </p>
+              </div>
+              <Separator />
+              <div className="flex items-center">
+                <label className="w-1/2 text-sm font-medium text-muted-foreground">Street:</label>
+                <p className="w-1/2 text-sm">
+                  {" "}
+                  {property.address || "N/A"}
+                </p>
+              </div>
+              <Separator />
+              <div className="flex items-center">
+                <label className="w-1/2 text-sm font-medium text-muted-foreground">District:</label>
+                <p>{property.district}</p>
+              </div>
+              <Separator />
+              <div className="flex items-center">
+                <label className="w-1/2 text-sm font-medium text-muted-foreground">City:</label>
+                <p>{property.city}</p>
               </div>
             </div>
-          </CardContent>
-        </Card>
-      </div>
+        </CardContent>
+      </Card>
 
-      <div className="space-y-6">
+      {/* Property Image */}
       <Card className="gap-2 shadow-none">
-          <CardHeader>
-            <CardTitle>Property Images</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="flex items-center justify-center rounded-lg bg-secondary aspect-video">
-              <div className="text-center text-muted-foreground">
-                <p>No images available</p>
-                <Button variant="default" size="sm" className="mt-2">
-                  Upload Images
-                </Button>
-              </div>
+        <CardHeader>
+          <CardTitle>Property Images</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="flex items-center justify-center rounded-lg bg-secondary aspect-video">
+            <div className="text-center text-muted-foreground">
+              <p>No images available</p>
+              <Button variant="default" size="sm" className="mt-2">
+                Upload Images
+              </Button>
             </div>
-          </CardContent>
-        </Card>
+          </div>
+        </CardContent>
+      </Card>
+      <div className="space-y-6 lg:col-span-1"></div>
 
-        <Card className="gap-2 shadow-none">
-          <CardHeader>
-            <CardTitle>Location</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="flex items-center justify-center rounded-lg bg-secondary aspect-video">
-              <div className="text-center text-muted-foreground">
-                <p>Map view</p>
-                <p className="text-sm">{property.address}</p>
-                <p className="text-sm">{property.district}, {property.city}</p>
-              </div>
+      <Card className="gap-2 shadow-none">
+        <CardHeader>
+          <CardTitle>Location</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="flex items-center justify-center rounded-lg bg-secondary aspect-video">
+            <div className="text-center text-muted-foreground">
+              <p>Map view</p>
+              <p className="text-sm">{property.address}</p>
+              <p className="text-sm">
+                {property.district}, {property.city}
+              </p>
             </div>
-          </CardContent>
-        </Card>
-      </div>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
