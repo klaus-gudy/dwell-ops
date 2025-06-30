a30b9d1b1afaf6d0d3927f8295e762de90ff1e8d
Klaus-gudy <klausongudy@gmail.com>
2025-06-30 22:37:21 +0300

    Remove console log from TenantsPage for cleaner output

--- app/(protected)/tenant/page.tsx
+++ app/(protected)/tenant/page.tsx
@@ -4,7 +4,6 @@ import { getAllTenantsWithLeaseStatus } from "@/lib/db/tenant";
 
 export default async function TenantsPage() {
   const tenants = await getAllTenantsWithLeaseStatus();
-  console.log("Tasks:", tenants);
     return (
       <div className="flex flex-col gap-2 px-4 py-2 md:gap-2 md:py-4 lg:px-6">
         <DataTable data={tenants} columns={columns} />
